dc6292e519d2ad16a6ea26a62ef8b05f43f7ce2e
Paul Henrich <paul@henrich.me>
2015-03-28 18:01:09 -0400

    Following Carin's Markov bot tutorial

---
+++ src/markov_elear/generator.clj
@@ -0,0 +1,45 @@
+(ns markov-elear.generator)
+
+(def example "And the Golden Grouse And the Pobble who")
+(def words (clojure.string/split example #" "))
+
+(defn word-transitions [sample]
+  (let [words (clojure.string/split sample #"[\s|\n]")]
+    (partition-all 3 1 words)))
+
+(defn word-chain [partitions]
+  (reduce (fn [r t] (merge-with clojure.set/union r
+                                 (let [[a b c] t]
+                                   {[a b] (if c #{c} #{})})))
+           {}
+           partitions))
+
+(defn text->word-chain [text]
+  (-> text
+      word-transitions
+      word-chain))
+
+(defn chain->text [chain]
+  (apply str (interpose " " chain)))
+
+(chain->text ["and" "the" "pobble"])
+
+(defn walk-chain [prefix chain result]
+  (let [suffixes (get chain prefix)]
+    (if (empty? suffixes)
+      result
+      (let [suffix (first (shuffle suffixes))
+            new-prefix [(last prefix) suffix]
+            result-with-spaces (chain->text result)
+            result-char-count (count result-with-spaces)
+            suffix-char-count (inc (count suffix))
+            new-result-char-count (+ result-char-count suffix-char-count)]
+        (if (> new-result-char-count 140)
+          result
+          (recur new-prefix chain (conj result suffix)))))))
+
+(defn generate-text [prefix chain]
+  (let [ prefix (clojure.string/split prefix #" ")
+        result-chain (walk-chain prefix chain prefix)
+        result-text (chain->text result-chain)]
+    result-text))
---
+++ test/markov_elear/generator_test.clj
@@ -0,0 +1,65 @@
+(ns markov-elear.generator-test
+  (:require [clojure.test :refer :all]
+            [markov-elear.generator :refer :all]))
+
+
+(deftest test-word-chain
+  (testing "it produces a chain of the possible two step transitions"
+    (let  [example '(("And" "the" "Golden")
+                     ("the" "Golden" "Grouse")
+                     ("And" "the" "Pobble")
+                     ("the" "Pobble" "who"))]
+      (is (= {["the" "Pobble"] #{"who"}
+              ["the" "Golden"] #{"Grouse"}
+              ["And" "the"] #{"Pobble" "Golden"}}
+             (word-chain example))))))
+
+(deftest test-text->word-chain
+  (testing "transforms string with spaces and newlines"
+    (let [example "And the Golden Grouse\nAnd the Pobble who"]
+      (is (= {["who" nil] #{}
+              ["Pobble" "who"] #{}
+              ["the" "Pobble"] #{"who"}
+              ["Grouse" "And"] #{"the"}
+              ["Golden" "Grouse"] #{"And"}
+              ["the" "Golden"] #{"Grouse"}
+              ["And" "the"] #{"Pobble" "Golden"}}
+             (text->word-chain example))))))
+
+;; copy & paste (shrug)
+(deftest test-walk-chain
+  (let [chain {["who" nil] #{},
+               ["Pobble" "who"] #{},
+               ["the" "Pobble"] #{"who"},
+               ["Grouse" "And"] #{"the"},
+               ["Golden" "Grouse"] #{"And"},
+               ["the" "Golden"] #{"Grouse"},
+               ["And" "the"] #{"Pobble" "Golden"}}]
+    (testing "dead end"
+      (let [prefix ["the" "Pobble"]]
+        (is (= ["the" "Pobble" "who"]
+               (walk-chain prefix chain prefix)))))
+    (testing "multiple options"
+      (with-redefs [shuffle identity]
+        (let [prefix ["And" "the"]]
+          (is (= ["And" "the" "Pobble" "who"]
+                 (walk-chain prefix chain prefix))))))
+    (testing "repeating chains"
+      (with-redefs [shuffle reverse]
+        (let [prefix ["And" "the"]]
+          (is (> 140
+                 (count (apply str (walk-chain prefix chain prefix)))))
+          (is (= ["And" "the" "Golden" "Grouse" "And" "the" "Golden" "Grouse"]
+                 (take 8 (walk-chain prefix chain prefix)))))))))
+
+(deftest test-generate-text
+  (with-redefs [shuffle (fn [c] c)]
+    (let [chain {["who" nil] #{}
+                 ["Pobble" "who"] #{}
+                 ["the" "Pobble"] #{"who"}
+                 ["Grouse" "And"] #{"the"}
+                 ["Golden" "Grouse"] #{"And"}
+                 ["the" "Golden"] #{"Grouse"}
+                 ["And" "the"] #{"Pobble" "Golden"}}]
+      (is (= "the Pobble who" (generate-text "the Pobble" chain)))
+      (is (= "And the Pobble who" (generate-text "And the" chain))))))
