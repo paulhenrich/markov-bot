ef0a016516702593c8fffaa6eb9b3afa70a26a0d
Paul Henrich <paul@henrich.me>
2015-04-03 19:29:10 -0400

    Fine tuning the generator.

--- src/markov_lean_shades/generator.clj
+++ src/markov_lean_shades/generator.clj
@@ -2,7 +2,12 @@
   (:require clojure.set))
 
 (def lean-words
-  #{"lean" "Lean" "Startup" "VC" "teams" "cross-functional" "startup" "branding" "entrepreneur" "explain" "experiment" "validate" "experiments"})
+  (let [ words #{"Ries" "Lean" "LSM" "adopting" "practices" "funnel" "growth" "hacking"
+                 "hacker" "VC" "teams" "cross-functional" "strategic" "strategy" "failure"
+                 "startup" "branding" "entrepreneur" "founder" "CTO" "disrupt" "MVP"
+                 "experiment" "experimental" "validate" "experiments"}]
+    (clojure.set/union words (map clojure.string/capitalize words))))
+
 (defn word-transitions [sample]
   (let [words (clojure.string/split sample #"[\s|\n]")]
     (partition-all 3 1 words)))
@@ -19,8 +24,8 @@
       word-transitions
       word-chain))
 
-(defn chain->text [chain]
-  (apply str (interpose " " chain)))
+(defn chain->text [[first-word & remaining]]
+  (apply str (clojure.string/capitalize first-word) " " (interpose " " remaining)))
 
 (defn walk-chain [prefix chain result]
   (let [suffixes (get chain prefix)]
@@ -63,40 +68,18 @@
   (rand-nth (keys (filter
                    (fn [el]
                      (= 0 (count (clojure.set/intersection lean-words (set (first el))))))
-                     lean-corpus))))
+                   lean-corpus))))
 
 (defn gen-random []
   (generate-text (chain->text (rand-prefix)) corpus))
 
 (defn up-to-n-random-phrases [n]
   (dotimes [i n]
-    (let [phrase (gen-random)]
-      (if (re-find #"\w \w.*(Lean|matrix|maker|hacker|startup|entrepreneur|analytics|branding|brand).*" phrase)
+    (let [phrase (gen-random)
+          score (count (clojure.set/intersection lean-words (-> (clojure.string/split phrase #"\s")
+                                                                 rest
+                                                                 rest
+                                                                 set)))]
+      (when (> score 0)
         (println phrase)))))
-
-(up-to-n-random-phrases 100)
-
-;;(filter (fn [el] (> (count (second el)) 2)) (process-file "fifty-shades.txt"))
-
-;; (generate-text "The Lean" corpus)
-;; (generate-text "powerful tool" corpus)
-;; (generate-text "The challenge" corpus)
-;; (generate-text "deliver value" corpus)
-;; (generate-text "Think goals" corpus)
-;; (generate-text "To validate" corpus)
-;; (generate-text "Powerful new" corpus)
-;; (generate-text "For example," corpus)
-;; (generate-text "The learning" corpus)
-;; (generate-text "No matter" corpus)
-;; (generate-text "experts may" corpus)
-;; (generate-text "best practices" corpus)
-;; (generate-text "lean startup" corpus)
-;; (generate-text "Startups live" corpus)
-;; (generate-text "I actually" corpus)
-;; (generate-text "The Lean" corpus)
-;; (generate-text "The trick" corpus)
-;; (generate-text "Bigger companies" corpus)
-;; (generate-text "SMBs that" corpus)
-;; (generate-text "the entrepreneur," corpus)
-;; (generate-text "entrepreneurs willing" corpus)
-;; (generate-text "adopting lean" corpus)
+(up-to-n-random-phrases 1000)
